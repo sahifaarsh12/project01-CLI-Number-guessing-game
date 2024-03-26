3a9396f63ca7bea1de0dd5b1e69ed4dd847787dc
sahifaarsh12 <donmoon260@gmail.com>
2024-03-26 21:08:29 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# project01-CLI-Number-guessing-game"  "# project01-CLI-Number-guessing-game" 
---
+++ index.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+//comp will generate a random number
+//user input for guessing number
+//compare user input with comp random number and show a result
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessednumber",
+        type: "number",
+        message: "Please guess a number between 1-6:",
+    },
+]);
+if (answers.userGuessednumber === randomNumber) {
+    console.log("Congratulations! you guessed right number ");
+}
+else {
+    console.log("You guessed wrong number");
+}
